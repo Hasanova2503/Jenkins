d47eb12748c106d40b1613089a29d823728c3e4d
Hasan Shahid <hasan.shahid@publicissapient.com>
2019-08-19 16:54:02 +0530

    Jenkins Test

---
+++ src/test/java/com/example/demo/HelloTest.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+	@Test
+	public void test() {
+		int a = 4 + 8;
+		assertEquals(12, a);
+	}
+
+}
